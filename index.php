08fa13dfedc2e8f8a4481fe4a1e28eb4a78822a7
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 11:05:27 +0100

    boucle 4

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les boucles</title>
+  </head>
+  <body>
+    <?php
+    $count=1;
+
+    while ($count <= 10) {
+      # code...
+      echo $count++ / $count ;
+    }
+
+     ?>
+  </body>
+</html>
